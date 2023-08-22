660e16994e42f3b706ab4e0477d41eeafbb15fd7
Sachin6096 <sachinkumararya29@gmail.com>
2023-08-22 03:07:09 -0700

    first attempt

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,27 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private boolean toppings;
+    private boolean ExtraCheese;
+
+    private boolean takeAway;
+
+    private boolean Bill;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        Bill = false;
+        toppings = false;
+        ExtraCheese= false;
+        takeAway = false;
+
+        if(isVeg)
+        {
+            this.price = 300;
+        }
+        else this.price = 400;
+        this.bill = "";
     }
 
     public int getPrice(){
@@ -17,18 +35,72 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!ExtraCheese && !Bill)
+        {
+            ExtraCheese = true;
+            this.price += 80;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!toppings && !Bill)
+        {
+            if(isVeg)
+            {
+                this.price += 70;
+            }
+            else this.price += 120;
+            toppings = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!takeAway && !Bill)
+        {
+            takeAway = true;
+            this.price += 20;
+        }
     }
 
     public String getBill(){
         // your code goes here
+
+        if(!Bill)
+        {
+            if(isVeg)
+            {
+
+                this.bill += "Base price of The Pizza: 300 \n";
+
+            }
+            else this.bill += "Base price of The Pizza: 400 \n";
+
+
+            if(ExtraCheese)
+            {
+                this.bill += "Extra Cheese Added: 80 \n";
+            }
+
+            if(toppings)
+            {
+                if(isVeg)
+                {
+                    this.bill += "Extra Toppings Added: 70 \n";
+                }
+                else this.bill += "Extra Toppings Added: 120";
+            }
+
+            if(takeAway)
+            {
+                this.bill += "Paperbag Added: 20 \n";
+            }
+
+            Bill = true;
+            this.bill += "Total Price:"+this.price;
+
+        }
         return this.bill;
     }
 }
